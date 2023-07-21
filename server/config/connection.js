da876c7d9bbb0db7c3aee99650bce81a4b60b100
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-21 07:48:57 -0700

    removed useCreateIndex. Deploying again to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,6 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  useCreateIndex: true,
 });
 
 mongoose.connection.on("connected", () => {
