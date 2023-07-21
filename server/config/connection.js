7e7684226021ec7c09b8bdf72f5160b6fdf2f80a
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-21 10:11:38 -0700

    changed atlas server

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI;
+const dbURI = MONGODB_URI;
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
